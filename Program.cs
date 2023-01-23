b40b2c32e6f46388fdcdb5a018a33754e00886c7
Grace Lee <gracelee9901@gmail.com>
2023-01-22 16:18:36 -0800

    Change Api response into JSON object

--- Program.cs
+++ Program.cs
@@ -2,16 +2,19 @@
 using System.Threading.Tasks;
 using RestSharp;
 using ConsoleApiCall.Keys;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ApiTest
 {
   class Program
   {
-    static void Main()
+    static void Main(string[] args)
     {
       Task<string> apiCallTask = ApiHelper.ApiCall(EnvironmentVariables.ApiKey);
       string result = apiCallTask.Result;
-      Console.WriteLine(result);
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Console.WriteLine(jsonResponse["results"]);
     }
   }
 
@@ -19,7 +22,7 @@ namespace ApiTest
   {
     public static async Task<string> ApiCall(string apiKey)
     {
-      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/vs");
+      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
       RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.Get);
       RestResponse response = await client.ExecuteAsync(request);
       return response.Content;
